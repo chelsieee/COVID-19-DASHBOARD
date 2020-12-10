4a16f4d6c8ef5531c78ca0df31c2c0810ce4a5c2
ChelsieFu <chensifu4@gmail.com>
2020-12-10 23:02:36 +1100

    countrydetailsinitialattamp

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let continents = []
-
+let country =[]
 $.get(
     "https://corona.lmao.ninja/v2/continents?yesterday=true&sort",
     (data) => {
@@ -9,14 +9,14 @@ $.get(
     }
 )
 
-const appendCardtoContainer = (element) => {
-    let card = createCard(element);
+const appendCardtoContainer = (element, i) => {
+    let card = createCard(element, i);
     const cardType = $(`<div class="col-md-4 col-sm-6 mb-4"></div>`)
     cardType.append(card)
     $('#container').append(cardType)
 }
 
-function createCard(element) {
+function createCard(element, i) {
     const name = element.continent
     const continentActiveCase =`<li>ActiveCases: ${element.active}</li>`
     const continentNewCase = `<li>NewCases: ${element.todayCases}</li>`
@@ -30,13 +30,62 @@ function createCard(element) {
     const continentheader =$(`<div class="card-header">${name}</div>`)
     const continentBody =$(`<div class="card-body"></div>`)
     const continentInfo =$(`<div class="card-info"></div>`)
+    const continentButton = $(`<button type="button" class="detailButton">Show More</button>`)
+    continentButton.click((event) =>{
+        let countryList = element.countries
+        console.log(countryList)
+        countryList.forEach((country) =>{
+            const countryName =(`<option value = ${country}>${country}</option>`)
+            $('#countries').append(countryName)
+            
+        });
+    });
 
     continentBody.append(continentActiveCase, continentNewCase, continentNewDeath, continentNewRecovered, continentTested, continentDeathRatio, continentRecovered)
+    continentInfo.append(continentButton)
     continentDiv.append(continentheader, continentBody, continentInfo)
     return continentDiv
 }
 
+$('#country').on("submit", (event) =>{
+    event.preventDefault();
+    console.log('print')
+        const countryDetails = $.get("https://corona.lmao.ninja/v2/countries?yesterday=&sort=", (details) =>{
+            country =details;
+            console.log(details)
+            let countryData = $('#countries option:selected').val()
+            console.log(countryData)
+            details.forEach((element) =>{
+                if(element.country == countryData){
+                let countryCard =createCardforCountry(element);  
+                $('#container2').append(countryCard);
+              }
+            }
+            )
+
+        }
+    )
+})
 
 
 
 
+function createCardforCountry(element) {
+    const name = element.country
+    const newCase = `<li>NewCases: ${element.todayCases}</li>`
+    const totalCase = `<li>TotalCases: ${element.Cases}</li>`
+    const newDeath = `<li>NewDeath: ${element.todayDeaths}</li>`
+    const totalDeath = `<li>TotalDeath: ${element.Deaths}</li>`
+    const newRecovered = `<li>NewRecovered: ${element.todayRecovered}</li>`
+    const Recovered = `<li>TotalRecovered: ${element.recovered}</li>`
+
+    const countryDiv = $(`<div class="card"></div`)
+    const countryHeader =$(`<div class="card-header">${name}</div>`)
+    const countryBody =$(`<div class="card-body"></div>`)
+    const countryInfo =$(`<div class="card-info"></div>`)
+
+    countryBody.append(newCase, totalCase, newDeath, totalDeath, newRecovered, Recovered);
+    countryDiv.append(countryHeader, countryBody, countryInfo);
+    return countryDiv;
+
+    };
